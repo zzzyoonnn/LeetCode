3085bc9b0c16514400fbe64414ffbea1d42b4f7c
Jiyoon Shin <118426807+zzzyoonnn@users.noreply.github.com>
2024-04-15 00:44:03 +0900

    Time: 9 ms (57.11%), Space: 46.4 MB (63.55%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        
+        ArrayList<int[]> answer = new ArrayList<>();
+        Arrays.sort(intervals, (x, y) -> x[0] - y[0]);
+        
+        int preStart = intervals[0][0];
+        int preEnd = intervals[0][1];
+        
+        for (int[] interval : intervals) {
+            int curStart = interval[0];
+            int curEnd = interval[1];
+            
+            if (curStart <= preEnd) {
+                //preStart = Math.min(curStart, preStart);
+                preEnd = Math.max(curEnd, preEnd);
+            } else {
+                answer.add(new int[]{preStart, preEnd});
+                
+                preStart = curStart;
+                preEnd = curEnd;
+            }
+        }
+        
+        answer.add(new int[]{preStart, preEnd});
+        return answer.toArray(new int[0][]);
+        
+    }
+}

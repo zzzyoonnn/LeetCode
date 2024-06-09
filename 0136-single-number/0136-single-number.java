9bd8dd91a83f78af5aa4f5f15aa10681830e2521
Jiyoon Shin <118426807+zzzyoonnn@users.noreply.github.com>
2024-06-10 00:19:17 +0900

    Time: 1 ms (99.91%), Space: 46 MB (37.98%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,18 +1,10 @@
-import java.util.*;
-
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();    // Key: element, Value: count
+        int result = 0;
         
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-        
-        int result = 0;
-        for (int n : map.keySet()) {
-            if (map.get(n) == 1) {
-                result = n;
-            }
+            // XOR 연산 사용(a ^ a = 0, a ^ 0 = a)
+            result = result ^ nums[i];
         }
         
         return result;

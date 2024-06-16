3ebce44d6664729e65bb181af5fff6c15886b845
Jiyoon Shin <118426807+zzzyoonnn@users.noreply.github.com>
2024-06-17 00:46:06 +0900

    Time: 1 ms (79.30%), Space: 41 MB (92.08%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] arr = s.split(" ");
+        
+        if (pattern.length() != arr.length) return false;
+        
+        HashMap<Character, String> map = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+
+            if (!map.containsKey(c)) {
+                for (char ch : map.keySet()) {
+                    if (map.get(ch).equals(arr[i])) return false;
+                }
+                map.put(c, arr[i]);
+            }
+            else {
+                if (!map.get(c).equals(arr[i])) return false;
+            }
+        }
+        return true;
+    }
+}
